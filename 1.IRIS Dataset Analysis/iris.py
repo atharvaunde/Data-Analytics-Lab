db826fbeb19f493b252441bf88bf6e1cd04f5c3a
Atharva Unde <contact@codekart.page>
2019-07-01 09:53:35 +0530

    Added Scattered Graph

--- 1.IRIS Dataset Analysis/iris.py
+++ 1.IRIS Dataset Analysis/iris.py
@@ -1,27 +1,58 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
-path="iris.csv"
+path = "iris.csv"
 
-df=pd.read_csv(path,header=None)
-headers=["Sepal-length","Sepal-width","Petal-length","Petal-width","Species"]
-df.columns=headers
-print(df.head(5)) #first 5 records when parameter is gives 
-print(df.tail(5)) #last 5 records when parameter is gives
-print(df.info())  #total records when parameter is gives
-print(df.dtypes)  #features 
+df = pd.read_csv(path, header=None)
+
+headers = ["Sepal-length", "Sepal-width", "Petal-length", "Petal-width", "Species"]
+df.columns = headers
+
+print(df.head())
+print(df.tail())
+print(df.info())
 print(df.shape)
- 
-print(df.describe()) #gives mean, min, max of feaures
-#print(df.describe(include = 'all')) #gives mean, min, max of ALL features
-d=df.drop(['Species'],axis=1)
+print(df.dtypes)
+print(df.describe())
 
-d.hist()
+df.hist()
 plt.show()
-plt.scatter('Sepal-length', 'Sepal-width', 'Petal-length', 'Petal-width', 'Species') #drawing the scattered graph
-plt.title('LAB 1')
-plt.xlabel('x') #defining the X axis for graph
-plt.ylabel('y') #defining the Y axis for graph
+
+df.boxplot()
 plt.show()
-plt.scatter (df ['Sepal-length'],df['Sepal-width'])
+
+plt.scatter(df["Sepal-length"], df["Sepal-width"])
+plt.show()
+
+plt.scatter(df["Sepal-length"], df["Petal-length"])
+plt.show()
+
+plt.scatter(df["Sepal-length"], df["Petal-width"])
+plt.show()
+
+plt.scatter(df["Sepal-width"], df["Sepal-length"])
+plt.show()
+
+plt.scatter(df["Sepal-width"], df["Petal-length"])
+plt.show()
+
+plt.scatter(df["Sepal-width"], df["Petal-width"])
+plt.show()
+
+plt.scatter(df["Petal-length"], df["Sepal-length"])
+plt.show()
+
+plt.scatter(df["Petal-length"], df["Sepal-width"])
+plt.show()
+
+plt.scatter(df["Petal-length"], df["Petal-width"])
+plt.show()
+
+plt.scatter(df["Petal-width"], df["Sepal-length"])
+plt.show()
+
+plt.scatter(df["Petal-width"], df["Sepal-width"])
+plt.show()
+
+plt.scatter(df["Petal-width"], df["Petal-length"])
 plt.show()
