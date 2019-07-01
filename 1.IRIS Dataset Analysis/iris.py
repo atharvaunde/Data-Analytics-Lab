f75baf99945df2bb8ea4042a651ff125932b88cd
Atharva Unde <contact@codekart.page>
2019-07-01 10:30:05 +0530

    Added Label for Scattered Graphs

--- 1.IRIS Dataset Analysis/iris.py
+++ 1.IRIS Dataset Analysis/iris.py
@@ -22,37 +22,64 @@ df.boxplot()
 plt.show()
 
 plt.scatter(df["Sepal-length"], df["Sepal-width"])
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
 plt.show()
 
 plt.scatter(df["Sepal-length"], df["Petal-length"])
+plt.xlabel('Sepal Length')
+plt.ylabel('Petal Width')
+
+
 plt.show()
 
 plt.scatter(df["Sepal-length"], df["Petal-width"])
+plt.xlabel('Sepal Length')
+plt.xlabel('Petal Width')
 plt.show()
 
 plt.scatter(df["Sepal-width"], df["Sepal-length"])
+plt.xlabel('Sepal Width')
+plt.ylabel('Sepal Length')
 plt.show()
 
 plt.scatter(df["Sepal-width"], df["Petal-length"])
+plt.xlabel('Sepal Width')
+plt.ylabel('Petal Length')
 plt.show()
 
 plt.scatter(df["Sepal-width"], df["Petal-width"])
+plt.xlabel('Sepal Width')
+plt.ylabel('Petal Width')
 plt.show()
 
 plt.scatter(df["Petal-length"], df["Sepal-length"])
+plt.xlabel('Petal Length')
+plt.ylabel('Sepal Length')
 plt.show()
 
 plt.scatter(df["Petal-length"], df["Sepal-width"])
+plt.xlabel('Petal Length')
+plt.ylabel('Sepal Width')
 plt.show()
 
 plt.scatter(df["Petal-length"], df["Petal-width"])
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
 plt.show()
 
 plt.scatter(df["Petal-width"], df["Sepal-length"])
+plt.xlabel('Petal Width')
+plt.xlabel('Sepal Length')
 plt.show()
 
 plt.scatter(df["Petal-width"], df["Sepal-width"])
+plt.xlabel('Petal Width')
+plt.xlabel('Sepal Width')
 plt.show()
 
+
 plt.scatter(df["Petal-width"], df["Petal-length"])
+plt.xlabel('Petal Width')
+plt.yxlabel('Petal Length')
 plt.show()
